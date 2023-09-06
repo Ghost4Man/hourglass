4962c0946e9e87940694cddcb44843977c180d96
Ghost4Man <kosik.dan@gmail.com>
2023-09-07 00:12:11 +0200

    Add an editable row to the timeline view

--- TimelineGUI/src/lib/TimelineEditor.svelte
+++ TimelineGUI/src/lib/TimelineEditor.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { onMount } from 'svelte';
   import moment from "moment";
-  import { SvelteGantt, SvelteGanttTable, MomentSvelteGanttDateAdapter } from 'svelte-gantt';
+  import { SvelteGantt, SvelteGanttTable, MomentSvelteGanttDateAdapter, SvelteGanttExternal } from 'svelte-gantt';
   import type { SvelteGanttComponent } from 'svelte-gantt/types/gantt';
   import type { SvelteTask, TaskModel } from 'svelte-gantt/types/core/task';
 
@@ -34,14 +34,21 @@
       target: document.getElementById('gantt-view'), 
       props: {
         dateAdapter: new MomentSvelteGanttDateAdapter(moment),
-        rowHeight: 150,
-        rows: [{
-          id: 0,
-          label: "some day",
-          height: 100,
-          classes: ["row-disabled"],
-          enableDragging: false,
-        }],
+        rowHeight: 100,
+        rows: [
+          {
+            id: 0,
+            label: "raw data",
+            height: 100,
+            classes: ["row-disabled"],
+            enableDragging: false,
+          },
+          {
+            id: 1,
+            label: "editable",
+            height: 100,
+          },
+        ],
         from: 0,
         to: 1,
         fitWidth: true,
@@ -54,17 +61,46 @@
       }
     });
     gantt.api.tasks.on.select(tasks => { selectedTasks = tasks; });
+    gantt.api.tasks.on.changed(_ => { selectedTasks = selectedTasks; });
+
+    new SvelteGanttExternal(document.getElementById('new-task-button'), {
+      gantt,
+      enabled: true,
+      dragging: false,
+      onsuccess: (row, date, gantt) => {
+        const id = 5000 + Math.floor(Math.random() * 99999);
+        gantt.updateTask({
+          id,
+          label: `Task #${id}`,
+          from: date,
+          to: moment(date).clone().add(30, 'minutes'),
+          classes: "has-unsaved-changes",
+          resourceId: row.model.id as number
+        });
+      },
+      // called when dragged outside main gantt area
+      onfail: () => { },
+      // factory function, creates HTMLElement that will follow the mouse
+      elementContent: () => {
+        const element = document.createElement('div');
+        element.innerHTML = 'New Task';
+        element.className = 'new-task-drag-ghost';
+        return element;
+      }
+    });
   })
 </script>
 
-<div id="gantt-view"></div>
+<div id="gantt-view">
+  <div id="new-task-button" class="sg-task">New task</div>
+</div>
 
 {#each (selectedTasks ?? []) as {model: task}}
   <aside class="card">
     <h3>{task.label}</h3>
     from: {moment(task.from).format("YYYY-MM-DD HH:mm:ss")}<br>
     to: {moment(task.to).format("YYYY-MM-DD HH:mm:ss")}<br>
-    duration: {moment.duration(task.from.diff(task.to)).humanize()}
+    duration: {moment.duration(moment(task.from).diff(task.to)).humanize()}
   </aside>
 {/each}
 
@@ -79,7 +115,9 @@
   #gantt-view :global(.row-disabled) {
     background: repeating-linear-gradient(45deg, #8883, #8883 10px, transparent 10px, transparent 20px);
   }
-  #gantt-view :global(.sg-task) {
+  
+  #gantt-view :global(.sg-task),
+  :global(.new-task-drag-ghost) {
     color: currentColor;
     background-color: #3da4fe75;
     white-space: wrap;
@@ -91,10 +129,14 @@
   #gantt-view :global(.sg-task):hover {
     background-color: #3da4fe99;
   }
+  #gantt-view :global(.sg-task.has-unsaved-changes) {
+    border: 3px dotted #fe3d60;
+  }
   #gantt-view :global(.sg-task.selected) {
-    border: 2px solid #3da4fe;
+    border-width: 3px;
     font-weight: bold;
   }
+
   #gantt-view :global(.sg-task-content) {
     padding: 0;
     position: unset;
@@ -106,11 +148,25 @@
     overflow: visible;
     width: auto;
   }
+
   #gantt-view :global(.column-header-cell) {
     border-color: #9997;
   }
   #gantt-view :global(.column-header-cell):hover {
     background-color: #9992;
+  }
+
+  #new-task-button {
+    width: 5em;
+    padding: 0.3em;
+    cursor: grab;
+  }
+
+  :global(.new-task-drag-ghost) {
+    position: absolute;
+    pointer-events: none;
+    cursor: grabbing;
+    padding: 0.3em;
   }
 </style>
 
